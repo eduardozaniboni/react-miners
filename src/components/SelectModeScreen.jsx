ec787fc5d351a9a56ed91a07d09565789409dbf7
Eduardo <eduardowz.prog@gmail.com>
2023-07-21 18:53:37 -0300

    feat: added new component SelectModeScreen

---
+++ src/components/SelectModeScreen.jsx
@@ -0,0 +1,25 @@
+import ButtonDescription from './ButtonDescription'
+import './SelectModeScreen.css'
+
+const SelectMode = ({ modes, modeSelected }) => {
+  return (
+    <div className='select'>
+      <div>
+        <h1>Selecione o Modo de Jogo</h1>
+        <p>Ao selecionar um dos modos de jogo abaixo, ele irá influenciar na quantidade de bombas que será gerada aleatoriamente no campo.</p>
+      </div>
+      <div className='buttonDesc'>
+        {modes.map((mode) => (
+          <ButtonDescription
+            key={mode.id}
+            name={mode.name}
+            percentage={mode.percentage}
+            modeSelected={() => modeSelected(mode.id, mode.percentage)}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default SelectMode
