911f1f76fa536249b771c35cee9425435b6b5b93
Eduardo <eduardowz.prog@gmail.com>
2023-07-20 23:10:15 -0300

    feat: added a useState hook to change between pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import { useState } from 'react'
 import './App.css'
+import StartScreen from './components/StartScreen'
+import Game from './components/Game'
+import GameOver from './components/GameOver'
+
+const stages = [
+  { id: 1, name: 'startWindow' },
+  { id: 2, name: 'gameWindow' },
+  { id: 3, name: 'endWindow' }
+]
 
 function App() {
 
+  const [gameStage, setGameStage] = useState(stages[0].name)
+
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
+
+  const endGame = () => {
+    setGameStage(stages[2].name)
+  }
+
+  const resetGame = () => {
+    setGameStage(stages[0].name)
+  }
+
   return (
     <div className='App'>
-      <h1>React Miners</h1>
+      {gameStage === 'startWindow' && <StartScreen startGame={startGame} />}
+      {gameStage === 'gameWindow' && <Game endGame={endGame} />}
+      {gameStage === 'endWindow' && <GameOver resetGame={resetGame} />}
     </div>
   )
 }
