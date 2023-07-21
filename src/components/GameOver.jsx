179fd838aa4a37616f05071ef7f436cbb1964f2a
Eduardo <eduardowz.prog@gmail.com>
2023-07-21 18:56:04 -0300

    styles: created a template based in startScreen

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,7 +1,10 @@
-const GameOver = ({ resetGame }) => {
+import './GameOver.css'
+
+const GameOver = ({ resetGame, score }) => {
   return (
-    <div>
-      <h1>Game Over</h1>
+    <div className='resetScreen'>
+      <h1>Fim de Jogo!</h1>
+      <h2>A sua pontuação foi: <span>100</span></h2>
       <button onClick={resetGame}>Recomeçar Jogo!</button>
     </div>
   )
